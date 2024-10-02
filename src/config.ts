14e573e87524b87fbb88124d998e22ac505b92a0
Blitz-Cloud <ionutp626@gmail.com>
2024-10-02 18:00:26 +0300

    Fix: local build errors

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  //author: "Sat Naing",
-  //profile: "https://satnaing.dev/",
-  //desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  website: "https://blitzcloud.me/", // replace this with your deployed domain
+  author: "Ionut",
+  profile: "https://satnaing.dev/",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "BlitzCloud",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
